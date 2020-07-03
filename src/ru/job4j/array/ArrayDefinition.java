566a66cad3e81cc12ea7bdc97b0added927b70a9
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2020-07-03 09:38:27 +0300

    6.0.2. Заполнение массива. [#311422]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,13 @@ public class ArrayDefinition {
         short[] age = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivav Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Semen Sidorov";
+        names[3] = "Mihail Mihailov";
+        for (int i = 0; i < names.length ; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
