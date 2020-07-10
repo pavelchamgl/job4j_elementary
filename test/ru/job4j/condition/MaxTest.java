5cf34c1c640cb9f137359002ca2f02e170b2135a
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2020-07-10 09:03:12 +0300

    3. Перегрузить метод max для трех чисел [#311573]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,5 +10,15 @@ public class MaxTest {
     @Test
     public void max() {
         assertThat(Max.max(5, 2), is(5));
+    }
+
+    @Test
+    public void whenMaxIsNine() {
+        assertThat(Max.max(5, 2, 9), is(9));
+    }
+
+    @Test
+    public void whenMaxIsTwenty() {
+        assertThat(Max.max(5, 2, 9, 20), is(20));
     }
 }
